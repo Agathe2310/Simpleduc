9dd3f45966a48e8187b25d54e20ff0c2ce0e97fc
root <clement.parisot@epsi.fr>
2023-01-05 17:27:04 +0100

    Module add, supp et liste

--- src/controller/AddEntrepriseController.php
+++ src/controller/AddEntrepriseController.php
@@ -6,7 +6,7 @@ function addRenduController($twig,$db){
     if((!empty($_POST['renduGroupe'])) && (!empty($_POST['trip-end'])) && (!empty($_POST['trip-start'])) && (!empty($_POST['renduLabel'])) && (!empty($_POST['renduDescription'])) && (isset($_POST['btnAddRendu']))){ ##determiner si le tableau est definit btnAddProduct nom du bouton on regarde s'il existe lors d'un envoi de formulaire
         $label = htmlspecialchars($_POST['renduLabel']);
         
-        saveRendu($db,$label,$description,$idgroupe,$prof,$datecreation,$dateline);
+        //saveRendu($db,$label,$description,$idgroupe,$prof,$datecreation,$dateline);
     
     }else{
         if(isset($_POST['btnAddRendu'])==true){
--- src/model/ProjetModel.php
+++ src/model/ProjetModel.php
@@ -8,3 +8,63 @@ function addPersonne($db, $nom, $prenom)
         'Prenom' => $prenom
     ]);
 }
+
+function addModule($db, $etat, $equipe, $contrat)
+{
+    $query = $db->prepare("INSERT INTO Module (Etat, IDEquipe, IDContrat) VALUES (:Etat, :IDEquipe, :IDContrat)");
+    return $query->execute([
+        'Etat' => $etat,
+        'IDEquipe' => $equipe,
+        'IDContrat' => $contrat
+    ]);
+}
+
+function suppModule($db, $id)
+{
+    $query = $db->prepare("DELETE FROM Module WHERE :IDModule = IDModule");
+    return $query->execute([
+        'IDModule' => $id
+    ]);
+}
+
+function addTache($db, $libelle, $etat, $dateDebut, $dateFin)
+{
+    $query = $db->prepare("INSERT INTO Module (Etat, IDEquipe, IDContrat) VALUES (:Etat, :IDEquipe, :IDContrat)");
+    return $query->execute([
+        'Etat' => $etat,
+        'IDEquipe' => $dateDebut,
+        'IDContrat' => $dateFin
+    ]);
+}
+
+function getAllModules($db)
+{
+    $query = $db->prepare("SELECT IDModule, Etat, IDEquipe, IDContrat FROM Module");
+    $query->execute([]);
+
+    $product = $query->fetchAll();
+
+    return $product;
+}
+
+
+
+function getAllEquipes($db)
+{
+    $query = $db->prepare("SELECT IDEquipe, IDPersonne FROM Equipe");
+    $query->execute([]);
+
+    $product = $query->fetchAll();
+
+    return $product;
+}
+
+function getAllContrats($db)
+{
+    $query = $db->prepare("SELECT IDContrat, IDEntre FROM Contrat");
+    $query->execute([]);
+
+    $product = $query->fetchAll();
+
+    return $product;
+}

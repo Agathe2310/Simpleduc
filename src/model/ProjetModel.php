70cb0674659be0247237643ae54ab2b44c205d4a
Agathe <agathe.poteaux@epsi.fr>
2023-01-05 16:03:28 +0100

    Ajout de personne

--- src/model/ProjetModel.php
+++ src/model/ProjetModel.php
@@ -2,9 +2,20 @@
 
 function addPersonne($db, $nom, $prenom)
 {
+<<<<<<< Updated upstream
     $query = $db->prepare("INSERT INTO Personne (Nom, Prenom) VALUES (:Nom, :Prenom)");
     return $query->execute([
         'Nom' => $nom,
         'Prenom' => $prenom
     ]);
 }
+=======
+    $query = $db->prepare("INSERT INTO Personne (Nom, Prénom) VALUES (:nom, :prenom)");
+    return $query->execute([
+        'Nom' => $nom,
+        'Prénom' => $prenom,
+    ]);
+}
+
+function addContrat($db, $DateSignature, $CoutGlobal, $DateDebut, $DateFin, $IDPersonne, $IDEntre)
+>>>>>>> Stashed changes

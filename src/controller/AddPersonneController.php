84be2b1ea312d3f8c2bd794dd681db6fa9702418
Agathe <agathe.poteaux@epsi.fr>
2023-01-26 15:40:42 +0100

    difference entre contacts et devs

--- src/controller/AddPersonneController.php
+++ src/controller/AddPersonneController.php
@@ -3,11 +3,22 @@ function addPersonneController($twig, $db) {
     include_once '../src/model/ProjetModel.php';
     
 
-    if (isset($_POST['btnAddPersonne'])) {
+    if (isset($_POST['btnAddDev'])) {
         $nom = htmlspecialchars($_POST["nom"]);
         $prenom = htmlspecialchars($_POST["prenom"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $password = htmlspecialchars($_POST["password"]);
 
-        addPersonne($db, $nom, $prenom);
+        addDev($db, $nom, $prenom, $email, password_hash($password, PASSWORD_DEFAULT));
+
+    }
+    if (isset($_POST['btnAddClient'])) {
+        $nom = htmlspecialchars($_POST["nom"]);
+        $prenom = htmlspecialchars($_POST["prenom"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $password = htmlspecialchars($_POST["password"]);
+
+        addClient($db, $nom, $prenom, $email, password_hash($password, PASSWORD_DEFAULT));
 
     }
 
